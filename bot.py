ff239a2d796c8e2c5934e967c450a9a40d004a68
quicktopup29-lab <quicktopup29@gmail.com>
2026-06-05 18:53:09 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
 import os
+import asyncio
 from telegram.ext import Application
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-def main():
+async def main():
+    if not TOKEN:
+        print("BOT_TOKEN not found!")
+        return
+
     app = Application.builder().token(TOKEN).build()
 
     print("TradeVision AI Bot Running...")
 
-    app.run_polling()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    while True:
+        await asyncio.sleep(60)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
